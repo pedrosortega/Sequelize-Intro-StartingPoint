1cf3b7ed88f422312e03d1f5fb31e109740faaec
pedrosortega <psortega2004@gmail.com>
2025-07-01 18:30:04 -0400

    made secure connection

--- database/db.js
+++ database/db.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
-// const pg = require("pg");
+const pg = require("pg");
 
 const db = new Sequelize(
-  "todo_list",
-  "postgres",
-  "poolrec",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   // process.env.DATABASE_URL || "postgres://localhost:5432/todo_list",
   {
-    host: "127.0.0.1",
-    dialect: "postgres",
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     logging: false, // comment this line to enable logging
   }
 );
